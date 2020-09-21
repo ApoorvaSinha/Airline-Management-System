e22927a24e3a89e6e7f92f7e9b727926ec7a83bb
ApoorvaSinha <apoorvasinha701@gmail.com>
2020-09-21 20:36:08 +0530

    commit8

--- jpa3/src/com/airline/service/FlightService.java
+++ jpa3/src/com/airline/service/FlightService.java
@@ -1,12 +1,14 @@
- package com.airline.service;
+package com.airline.service;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import com.airline.models.Airplane;
 import com.airline.models.Flight;
+import com.airline.models.Pilot;
 
 /**
  * Session Bean implementation class FlightService
@@ -30,6 +32,16 @@ public class FlightService {
 		em.persist(f);
 		em.persist(a);
 
+	}
+
+	public void addPilotToFlight(String pilotId, String flightId) {
+
+		TypedQuery<Flight> fQuery = em.createNamedQuery("Flight.findById", Flight.class);
+		fQuery.setParameter("id", Integer.parseInt(flightId));
+		Flight f = fQuery.getSingleResult();
+		TypedQuery<Pilot> pQuery = em.createNamedQuery("Pilot.findById", Pilot.class);
+		pQuery.setParameter("id", Integer.parseInt(pilotId));
+		Pilot p = pQuery.getSingleResult();
 	}
 
 }
