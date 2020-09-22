14ca9d2e4327c86ae99d4edd190bedcf5740ff4c
ApoorvaSinha <apoorvasinha701@gmail.com>
2020-09-22 19:36:38 +0530

    commit18

--- jpa3/src/com/airline/service/PassengerService.java
+++ jpa3/src/com/airline/service/PassengerService.java
@@ -1,9 +1,12 @@
 package com.airline.service;
 
+import java.util.List;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import com.airline.models.Passenger;
 
@@ -26,5 +29,12 @@ public class PassengerService {
 	
 	public void addPassenger(Passenger p) {
 		em.persist(p);
+	}
+	
+	public List<Passenger> getPassengers(){
+		
+		TypedQuery<Passenger> query= em.createQuery("SELECT p FROM Passenger p", Passenger.class);
+		List<Passenger> pList = query.getResultList();
+		return pList;
 	}
 }
